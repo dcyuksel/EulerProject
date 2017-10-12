cbea31e43f6fcaca4b8e4d560fa915ce33b00e5c
DenizCan <dnzcnyksl@gmail.com>
2017-10-12 22:56:11 +0300

    project15 implemented

---
+++ Project15/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Project15
+{
+    using System;
+    using System.Linq;
+    using System.Numerics;
+
+    public class Program
+    {
+        private static readonly BigInteger modulo = 1000000007;
+
+        public static void Main(string[] args)
+        {
+            var numberOfTestCases = Convert.ToInt32(Console.ReadLine());
+
+            for (var i = 0; i < numberOfTestCases; i++)
+            {
+                var inputArray = Console.ReadLine().Split(' ');
+                var n = BigInteger.Parse(inputArray.ElementAt(0));
+                var m = BigInteger.Parse(inputArray.ElementAt(1));
+
+                Console.WriteLine((Factorial(n + m) / (Factorial(n) * Factorial(m))) % modulo); 
+            }
+        }
+
+        public static BigInteger Factorial(BigInteger number)
+        {
+            if (number == 1)
+            {
+                return 1;
+            }
+
+            return number * Factorial(number - 1);
+        }
+    }
+}
